e460b050475ed4b5b05c09e7f65744dc784c84cc
Daniel Moretto <100226246+dnl333@users.noreply.github.com>
2022-07-20 13:55:55 -0300

    Projeto completo

---
+++ script.js
@@ -0,0 +1,40 @@
+let inputadultos = document.getElementById("adultos")
+let inputcriancas = document.getElementById("criancas")
+let inputduracao = document.getElementById("duracao")
+
+let resultado = document.getElementById("resultado")
+
+function calcular(){
+
+let adultos = inputadultos.value
+let criancas = inputcriancas.value
+let duracao = inputduracao.value
+
+let qdtTotalCarne = carnepp(duracao) * adultos + (carnepp(duracao) / 2 * criancas)
+
+let qdtTotalCerveja = cervejapp(duracao) * adultos
+
+let qdtTotalBebidas = bebidaspp(duracao) * adultos + (bebidaspp(duracao) / 2 * criancas)
+
+resultado.innerHTML=`<p>${qdtTotalCarne/1000} Kg de Carne </p> `
+resultado.innerHTML+=`<p>${Math.ceil(qdtTotalCerveja/355)} Latas de Cerveja </p> `
+resultado.innerHTML+=`<p>${Math.ceil(qdtTotalBebidas/2000)} Garrafas de Refrigerantes</p> `
+}
+
+function carnepp(duracao){
+
+    if(duracao>=6){return 650}
+    else {return 400}
+}
+
+function cervejapp(duracao){
+
+    if(duracao>=6){return 2000}
+    else {return 1200}
+}
+
+function bebidaspp(duracao){
+
+    if(duracao>=6){return 1500}
+    else {return 1000}
+}
